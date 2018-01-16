893a44a4f75fe1f5edb6f40761a460643caf7f26
annabytnar <akbytnar@gmail.com>
2018-01-16 14:04:23 +0100

    modal

---
+++ src/main/java/edu/agh/bazyprojekt/javascript/orders.js
@@ -0,0 +1,85 @@
+var i = 0;
+var orders = [
+    {
+        id: 1223,
+        customerId: 'nanan',
+        employeeId: 213432,
+        orderDate: 22,
+        requiredDate: 32,
+        shippedDate: 23,
+        shipVia: 23,
+        freight: 32,
+        shipName: 324114324,
+        shipAdress: 0.3,
+        shipCity: 'Krakow',
+        shipRegion: 'Europa',
+        shipPostalCode: '23eeqw',
+        shipCountry: 'Polska'
+    },
+    {
+        id: 1,
+        customerId: 'nanan',
+        employeeId: 213432,
+        orderDate: 22,
+        requiredDate: 32,
+        shippedDate: 23,
+        shipVia: 23,
+        freight: 32,
+        shipName: 324114324,
+        shipAdress: 0.3,
+        shipCity: 'Krakow',
+        shipRegion: 'Europa',
+        shipPostalCode: '23eeqw',
+        shipCountry: 'Polska'
+    },
+    {
+        id: 23,
+        customerId: 'nanan',
+        employeeId: 213432,
+        orderDate: 22,
+        requiredDate: 32,
+        shippedDate: 23,
+        shipVia: 23,
+        freight: 32,
+        shipName: 324114324,
+        shipAdress: 0.3,
+        shipCity: 'Krakow',
+        shipRegion: 'Europa',
+        shipPostalCode: '23eeqw',
+        shipCountry: 'Polska'
+    }
+];
+
+var ordersMap = new Map();
+for (i = 0; i < orders.length; i++) {
+    ordersMap.set(orders[i].id, orders[i]);
+}
+
+function openOrderModal(orderId) {
+
+    var order = ordersMap.get(orderId);
+    $('#idOrder').val(order.id);
+    $('#customerId').val(order.customerId);
+    $('#employeeId').val(order.employeeId);
+    $('#orderDate').val(order.orderDate);
+    $('#requiredDate').val(order.requiredDate);
+    $('#shipVia').val(order.shipVia);
+    $('#freight').val(order.freight);
+    $('#shipName').val(order.shipName);
+    $('#shipAdress').val(order.shipAdress);
+    $('#shipCity').val(order.shipCity);
+    $('#shipRegion').val(order.shipRegion);
+    $('#shipPostalCode').val(order.shipPostalCode);
+    $('#shipCountry').val(order.shipCountry);
+    $('#orderModal').modal('show');
+}
+
+var orderTable = document.getElementById('ordersTableBody');
+
+var text = '';
+for (i = 0; i < orders.length; i++) {
+    text = text + "<tr><th>" + orders[i].id + "</th><td>" + orders[i].customerId + "</td><td>" + orders[i].employeeId + "</td><td>" + orders[i].orderDate + "</td><td>" + orders[i].requiredDate + "</td><td>" + orders[i].shipVia + "</td><td>" + orders[i].freight + "</td><td>" + orders[i].shipName + "</td><td>" + orders[i].shipAdress + "</td><td>" + orders[i].shipCity + "</td><td>" + orders[i].shipRegion + "</td><td>" + orders[i].shipPostalCode + "</td><td>" + orders[i].shipCountry +"</td><td><button type=\"button\" class=\"btn btn-primary btn-sm\" onclick=\"openOrderModal(" + orders[i].id + ")\">\n" +
+        "            Edit\n" +
+        "        </button></td>";
+}
+orderTable.innerHTML = text;
--- src/main/java/edu/agh/bazyprojekt/javascript/products.js
+++ src/main/java/edu/agh/bazyprojekt/javascript/products.js
@@ -1,10 +1,12 @@
+var i = 0;
 var products = [
     {
         id: 1223,
         name: 'nanan',
         categoryId: 213432,
+        supplierId: 22,
         quantityPerUnit: 32,
-        unitPrice:23,
+        unitPrice: 23,
         unitsInStock: 23,
         unitsOnOrder: 32,
         reorderLevel: 324114324,
@@ -13,38 +15,55 @@ var products = [
     {
         id: 12,
         name: 'aaa',
+        supplierId: 22,
         categoryId: 213432,
         quantityPerUnit: 32,
-        unitPrice:23,
+        unitPrice: 23,
         unitsInStock: 23,
         unitsOnOrder: 32,
         reorderLevel: 324114324,
-        discontinued: 0.3
+        discontinued: 0.5
     },
     {
         id: 13,
         name: 'nnn',
         categoryId: 213432,
+        supplierId: 22,
         quantityPerUnit: 32,
-        unitPrice:23,
+        unitPrice: 23,
         unitsInStock: 23,
         unitsOnOrder: 32,
         reorderLevel: 324114324,
-        discontinued: 0.3
+        discontinued: 0.9
     }
 ];
 
+var productsMap = new Map();
+for (i = 0; i < products.length; i++) {
+    productsMap.set(products[i].id, products[i]);
+}
+
 function openProductModal(productId) {
-    var productModal = document.getElementById('productModalBody');
-    productModal.innerText = "<p>"+ productId +"</p>";
+
+    var product = productsMap.get(productId);
+    $('#id').val(product.id);
+    $('#name').val(product.name);
+    $('#categoryId').val(product.categoryId);
+    $('#supplierId').val(product.supplierId);
+    $('#quantityPerUnit').val(product.quantityPerUnit);
+    $('#unitPrice').val(product.unitPrice);
+    $('#unitsInStock').val(product.unitsInStock);
+    $('#unitsOnOrder').val(product.unitsOnOrder);
+    $('#reorderLevel').val(product.reorderLevel);
+    $('#discontinued').val(product.discontinued);
     $('#productModal').modal('show');
 }
 
 var productTable = document.getElementById('productsTableBody');
 console.log(productTable);
 var text = '';
-for(var i=0;i<products.length;i++){
-    text = text + "<tr><th>" + products[i].id +"</th><td>" + products[i].name + "</td><td>" +products[i].categoryId +"</td><td>" +products[i].quantityPerUnit +"</td><td>"+products[i].quantityPerUnit+"</td><td>"+products[i].quantityPerUnit+"</td><td>"+products[i].quantityPerUnit+"</td><td>"+products[i].quantityPerUnit+"</td><td>"+products[i].quantityPerUnit+"</td><td>"+products[i].discontinued+"</td><td><button type=\"button\" class=\"btn btn-primary btn-sm\" onclick=\"openProductModal("+products[i].id +")\">\n" +
+for (i = 0; i < products.length; i++) {
+    text = text + "<tr><th>" + products[i].id + "</th><td>" + products[i].name + "</td><td>" + products[i].supplierId + "</td><td>" + products[i].categoryId + "</td><td>" + products[i].quantityPerUnit + "</td><td>" + products[i].unitPrice + "</td><td>" + products[i].unitsInStock + "</td><td>" + products[i].unitsOnOrder + "</td><td>" + products[i].reorderLevel + "</td><td>" + products[i].discontinued + "</td><td><button type=\"button\" class=\"btn btn-primary btn-sm\" onclick=\"openProductModal(" + products[i].id + ")\">\n" +
         "            Edit\n" +
         "        </button></td>";
 }
