61910e43661c4a233579cc9388cb95810084b8eb
Adam Koscinski <kostekman1@gmail.com>
2018-01-19 19:51:08 +0100

    Added BasiGenericHibernateController as simple testing class.

---
+++ src/main/java/edu/agh/bazyprojekt/controller/BasicGenericHibernateController.java
@@ -0,0 +1,44 @@
+package edu.agh.bazyprojekt.controller;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class BasicGenericHibernateController<K> {
+
+    private final Session session;
+
+    public BasicGenericHibernateController(Session session){
+        this.session = session;
+    }
+
+    public void saveObjectsToDb(List<K> objects){
+        Transaction transaction = session.beginTransaction();
+
+        for(K object : objects) {
+            session.save(object);
+        }
+
+        transaction.commit();
+
+
+    }
+
+    public void removeObjectFromDb(List<K> objects){
+        Transaction transaction = session.beginTransaction();
+        for(K object : objects) {
+            session.delete(object);
+        }
+        transaction.commit();
+
+
+    }
+
+    private List<K> selectFromTable(String tableName){
+        Query<K> query = session.createQuery("FROM " + tableName);
+
+        return query.getResultList();
+    }
+}
